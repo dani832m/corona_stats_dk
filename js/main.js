885f1bce2b96b3c0e5a6c3079b7ddd5eb19d5d1e
Daniel Lyck <d9@live.dk>
2020-04-10 14:39:31 +0200

    Layout and responsiveness optimized.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 /* Get Data from API */
 const xmlhttp = new XMLHttpRequest();
 
-xmlhttp.onreadystatechange = function() {
+xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     const dataObj = JSON.parse(this.responseText);
     document.querySelector(".stats-today-confirmed").innerHTML = dataObj.data.today.confirmed;
     document.querySelector(".stats-today-deaths").innerHTML = dataObj.data.today.deaths;
     document.querySelector(".stats-total-confirmed").innerHTML = dataObj.data.latest_data.confirmed;
+    document.querySelector(".stats-total-critical").innerHTML = dataObj.data.latest_data.critical;
     document.querySelector(".stats-total-deaths").innerHTML = dataObj.data.latest_data.deaths;
     document.querySelector(".stats-total-recovered").innerHTML = dataObj.data.latest_data.recovered;
   }
@@ -26,8 +27,8 @@ sourceBtn.addEventListener("click", () => {
     footerTxt.style.bottom = "0";
     footerTxtVisible = true;
   } else {
-    footerTxt.style.visibility = "hidden";
-    footerTxt.style.bottom = "-20%";
+    footerTxt.style.visibility = "";
+    footerTxt.style.bottom = "";
     footerTxtVisible = false;
   }
 });
@@ -36,7 +37,7 @@ sourceBtn.addEventListener("click", () => {
 const footerCloseBtn = document.querySelector(".footer-close-button");
 
 footerCloseBtn.addEventListener("click", () => {
-  footerTxt.style.visibility = "hidden";
-  footerTxt.style.bottom = "-20%";
+  footerTxt.style.visibility = "";
+  footerTxt.style.bottom = "";
   footerTxtVisible = false;
 });
